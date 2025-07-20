cfd2501d19af1cb11fdb5932068cf921d8c48c77
Vikash kushwaha <152359232+vikash0064@users.noreply.github.com>
2025-07-20 20:36:59 +0530

    Create  Reverse Right-Aligned Hollow Triangle.cpp

---
+++ Reverse Right-Aligned Hollow Triangle.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+                                                                                                                                                            // /* Reverse Right-Aligned Hollow Triangle
+                                                                                                                                                            // *****
+                                                                                                                                                            // *  *
+                                                                                                                                                            // * *
+                                                                                                                                                            // **
+                                                                                                                                                            // *
+                                                                                                                                                            //*/
+                                                                                                                                                          
+int main() {
+    int n;
+    cin >> n;
+
+    for (int row = 0; row < n; row++) {
+        for (int col = 0; col < n - row; col++) {
+            if (row == 0 || row == n - 1) {
+                cout << "*";
+            } else {
+                if (col == 0 || col == n - row - 1) {
+                    cout << "*";
+                } else {
+                    cout << " ";
+                }
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
+
